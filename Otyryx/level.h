9d80a0a582871db52801b92b359dec0298870f1e
Languzda <langusda@gmail.com>
2021-05-30 14:56:51 +0200

    Create class Scene

--- Otyryx/level.h
+++ Otyryx/level.h
@@ -1,14 +1,33 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include "character.h"
 
 class Level
 {
 public:
     Level();
     void generate();
+    void draw();
+    ~Level();
 private:
-    int height,width;
+    std::vector<Actor *> actors_;
+    Hero *player_;
+};
+
+class Scene
+{
+private:
+    sf::RenderWindow window_;
+    Level *level_;
+
+public:
+    Scene(int width, int height); // Create window
+    void set_level(Level *ptr); //
+    void draw(); //draw scene
+    void events(sf::Event &event); //chceck events
+    void window_loop(); //window loop
+
 };
 
 class Terrain{
