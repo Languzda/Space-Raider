492f2506e166fceb995de59f9f88e719850e2396
Languzda <langusda@gmail.com>
2021-05-31 21:44:45 +0200

    change

--- Otyryx/character.cpp
+++ Otyryx/character.cpp
@@ -1,4 +1,5 @@
 #include "character.h"
+#include <iostream>
 
 Actor::Actor(std::string texture_path, float scale,sf::IntRect rect, int x , int y, bool is_Repeated = false){
     if(texture_.loadFromFile(texture_path)){
@@ -8,9 +9,43 @@ Actor::Actor(std::string texture_path, float scale,sf::IntRect rect, int x , int
         texture_.setRepeated(is_Repeated);
         this->setPosition(x,y);
     }
+    else{
+        std::cout<<"blad tekstury"<<std::endl;
+    }
 }
+
 
 Character::Character(std::string texture_path, float scale,sf::IntRect rect,
                      int x , int y, bool is_Repeated = false,int hp=0) :
     Actor(texture_path,scale,rect,x,y,is_Repeated),health_points_(hp){}
 
+void Character::setHP(int hp){this->health_points_=hp;}
+
+int Character::getHP(){return health_points_;}
+
+Hero::Hero(std::string texture_path, float scale,sf::IntRect rect,
+           int x , int y, bool is_Repeated = false,int hp=0) : Character(texture_path,scale,rect,x,y,is_Repeated,hp){}
+
+Hero::~Hero(){}
+
+
+Enemy::Enemy(std::string texture_path, float scale, sf::IntRect rect,
+             int x , int y, bool is_Repeated = false, int hp=0, int veX=-25) : Character(texture_path,scale,rect,x,y,is_Repeated,hp),velocity_x(veX){}
+
+void Enemy :: move_e(){
+    this->move(velocity_x*1/30,0);
+}
+
+void Enemy::change_velocity(const Direction direction){
+    if(direction == Left) velocity_x = -std::abs(velocity_x);
+    if(direction == Right) velocity_x = std::abs(velocity_x);
+}
+
+PlayerBullet::PlayerBullet(std::string texture_path, float scale,sf::IntRect rect, int x , int y, bool is_Repeated,int veX,int veY)
+    :Actor(texture_path,scale,rect,x,y,is_Repeated),velocity_x(veX),velocity_y(veY){
+    this->setRotation(0.5);
+}
+
+void PlayerBullet::move_bullet(){
+    this->move(velocity_x*1/30,velocity_y*1/30);
+}
--- Otyryx/character.h
+++ Otyryx/character.h
@@ -6,19 +6,23 @@ class Actor : public sf::Sprite //klasa abstrakcyjna wszystkich animowanych obie
 {
 public:
     Actor(std::string texture_path, float scale,sf::IntRect rect, int x , int y, bool is_Repeated);
-    virtual ~Actor()=0;
+    virtual ~Actor()=default;
 private:
     sf::Texture texture_;
 };
+
+//Klasy postaci
 
 class Character : public Actor //klasa abstrakcyjna wszystkich postaci (dzidziczy z Actor)
 {
 public:
     Character(std::string texture_path, float scale,sf::IntRect rect,
               int x , int y, bool is_Repeated,int hp);
-    virtual ~Character();
-    virtual void attack();
+    virtual ~Character()=default;
+    //virtual void attack();
     void shot();
+    int getHP();
+    void setHP(int hp);
 protected:
     int health_points_;
 };
@@ -26,7 +30,9 @@ protected:
 class Hero : public Character //klasa postaci sterowanej przez gracza
 {
 public:
-    void movement();
+    Hero(std::string texture_path, float scale,sf::IntRect rect,
+         int x , int y, bool is_Repeated,int hp);
+    ~Hero();
     void setMultishot();
     void setBounce_ball();
 private:
@@ -35,20 +41,30 @@ private:
 
 };
 
-//class Melee : public Character
-//{
-//public:
-
-//};
-
-class Ranged : public Character //klasa przeciwnika
+enum Direction { Left , Right };
+class Enemy : public Character //klasa przeciwnika
 {
-
+public:
+    Enemy(std::string texture_path, float scale,sf::IntRect rect,
+         int x , int y, bool is_Repeated,int hp, int veX);
+    ~Enemy(){};
+    void move_e();
+    void change_velocity(const Direction direction);
+private:
+    int velocity_x;
 };
 
-class Bullet : public Actor //obiekt pocisk
+//Klasy obiektów
+
+class PlayerBullet : public Actor //obiekt pocisk
 {
-    ~Bullet();
+public:
+    PlayerBullet(std::string texture_path, float scale,sf::IntRect rect, int x , int y, bool is_Repeated,int veX,int veY);
+    ~PlayerBullet(){};
+    void move_bullet(/*sf::Time elapsed*/);
+private:
+    int velocity_x;
+    int velocity_y;
 };
 
 #endif // CHARACTER_H
